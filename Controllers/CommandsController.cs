d01f0d29add2e63dcb6171e15a811d8ddb0f3142
sheikhimtiaz <imtiaz.ahmed@selise.ch>
2021-09-03 02:42:58 +0600

    add logger for console and file

--- Controllers/CommandsController.cs
+++ Controllers/CommandsController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Identity.Data;
 using Identity.Models;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
 
 namespace Identity.Controllers
 {
@@ -21,13 +22,26 @@ namespace Identity.Controllers
         {
             var commandItems = _repository.GetAllCommands();
 
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("log-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Ah, there you are!");
+
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+
+            Log.Information("Processed {@Position} in {Elapsed:000} ms. \n {@commandItems}", position, elapsedMs, commandItems);
+            Log.CloseAndFlush();
+
             return Ok(commandItems);
         }
 
         [HttpGet("{id}")]
-        public ActionResult <User> GetCommandById(int ItemId)
+        public ActionResult <User> GetCommandById(int itemId)
         {
-            var commandItem = _repository.GetCommandById(ItemId);
+            var commandItem = _repository.GetCommandById(itemId);
 
             return Ok(commandItem);
         }
--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,6 @@
 # WORKDIR /app
 # EXPOSE 80
 
-
-
-
 FROM mcr.microsoft.com/dotnet/core/sdk:3.1 AS build-env
 WORKDIR /app
 
